29366672c2cc3c4db19050f17bf86be442a2cad0
msuhovec <suhovecky@nd.edu>
2017-01-25 14:37:22 -0500

    submit for wr-99

--- testlibndpysvc01/handler.py
+++ testlibndpysvc01/handler.py
@@ -4,7 +4,7 @@ import logging
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
-# this adds the component-level `lib` directory to the Python import path
+# this adds the component-level `liy` directory to the python import path
 import sys, os
 # get this file's directory independent of where it's run from
 here = os.path.dirname(os.path.realpath(__file__))
@@ -16,19 +16,31 @@ sys.path.append(os.path.join(here, "../vendored"))
 import lib
 
 
-def read(event, context):
-    log.debug("Received event {}".format(json.dumps(event)))
-    redirects = lib.all_redirects()
+def redirect(event, context):
+	log.debug("received event {}".format(json.dumps(event)))
+	redirects = lib.all_redirects()
 
-    body = {
-        "message": "What, me worry?",
-        "input": event['queryStringParameters']
-    }
+	statuscode = 302
+	headers = {}
+	message = 'url has been redirected'
 
-    response = {
-        "statusCode": 302,
-	"body": json.dumps(body)
-    }
+	body = {
+        	"message": message,
+        	"input": event['queryStringParameters']
+	}
+
+	if body['input'] == None or body['input']['redirect_code'] == None or redirects[body['input']['redirect_code']] == None:
+		statuscode = 404
+		body['message'] = "Redirect Code was Not Found"
+	else:
+		headers['Location'] = redirects[event['queryStringParameters']['redirect_code']] 
+
+	response = {
+		"statusCode": statuscode,
+		"body": json.dumps(body),
+		"headers": headers
+	}
 
 
-    return response
+
+	return response
